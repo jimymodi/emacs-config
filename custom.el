60d08e0bd12c81694ffef940b31ed53ae55378fa
Jimit Modi <jimit@kodeplay.com>
2012-09-27 19:25:33 +0530

    change theme from dark to light

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(background-color "#002b36")
  '(background-mode dark)
  '(cursor-color "#839496")
- '(custom-safe-themes (quote ("27b53b2085c977a8919f25a3a76e013ef443362d887d52eaa7121e6f92434972" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "935e766f12c5f320c360340c8d9bc1726be9f8eb01ddeab312895487e50e5835" default)))
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "935e766f12c5f320c360340c8d9bc1726be9f8eb01ddeab312895487e50e5835" default)))
  '(foreground-color "#839496")
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -14,4 +14,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier 10 Pitch" :foundry "bitstream" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Courier 10 Pitch" :foundry "bitstream" :slant normal :weight bold :height 113 :width normal)))))
--- init.el
+++ init.el
@@ -107,10 +107,10 @@
 ;; custom-theme-load-path is new in Emacs24
 (add-to-list 'custom-theme-load-path "~/emacs/site/themes")
 (add-to-list 'custom-theme-load-path "~/emacs/site/themes/solarized")
-(add-to-list 'custom-theme-load-path "~/emacs/site/themes/tomorrow-night")
+;;(add-to-list 'custom-theme-load-path "~/emacs/site/themes/tomorrow-night")
 
 ;; solarized-dark - current theme of choice
-(load-theme 'solarized-dark t)
+(load-theme 'solarized-light t)
 
 ;;;; On demand loading
 (load "~/emacs/site/ondemand.el")
